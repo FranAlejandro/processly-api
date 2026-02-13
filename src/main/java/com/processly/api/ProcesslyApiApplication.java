d633354d2f36d91bd475dd1c4fbcc3920792cc7a
Francisco Bernal <proyectoapoloiv@gmail.com>
2026-02-13 00:52:17 -0300

    feat: initial Spring Boot setup with PostgreSQL integration and User entity

---
+++ src/main/java/com/processly/api/ProcesslyApiApplication.java
@@ -0,0 +1,13 @@
+package com.processly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesslyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesslyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/processly/api/ProcesslyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processly.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesslyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
